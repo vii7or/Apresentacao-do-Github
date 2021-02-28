502679b2b4625502a9ecbbc26670952d461d9cdb
Vitor Pereira <79769685+vii7or@users.noreply.github.com>
2021-02-28 14:39:29 -0300

    Update Cliente.java
    adicionei cpf no cliente

--- Locacao/src/br/ubc/locadora/Cliente.java
+++ Locacao/src/br/ubc/locadora/Cliente.java
@@ -3,10 +3,12 @@ package br.ubc.locadora;
 public class Cliente {
 	String nome;
 	int idade;
-	
-	Cliente(String nome, int idade){
+	int cpf ;
+	Cliente(String nome, int idade , int cpf ) {
 		this.nome = nome;
 		this.idade = idade;
+		this.cpf = cpf;
+
 	}
 
 	
